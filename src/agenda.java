edfab9741db60af76313133f678a136d2d6158d9
Danny Vinueza <danny.vinueza@epn.edu.ec>
2023-02-07 10:28:29 -0500

    Nombre a los componentes

--- src/agenda.java
+++ src/agenda.java
@@ -3,10 +3,10 @@ import javax.swing.*;
 public class agenda {
     private JPanel pagendar;
     private JLabel titulo;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JButton buscarButton;
-    private JButton actualizarButton;
+    private JTextField cedula;
+    private JTextField nombre;
+    private JTextField apellido;
+    private JTextField email;
+    private JButton buscarB;
+    private JButton actualizarB;
 }
