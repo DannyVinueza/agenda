473e76e13ffc66633a35c035d1f9a1ab0dd16b63
Nestor <nestor.chumania@epn.edu.ec>
2023-02-07 10:42:57 -0500

    Coneccion

--- src/agenda.java
+++ src/agenda.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class agenda {
     private JPanel pagendar;
@@ -9,4 +12,34 @@ public class agenda {
     private JTextField email;
     private JButton buscarB;
     private JButton actualizarB;
+
+    public agenda(){
+        getConection();
+
+    }
+    public static Connection getConection(){
+        Connection con = null;
+        String url = "jdbc:mysql://localhost/Agenda",
+                user = "root",
+                password = "UGPCUGR2002";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println(e);
+        }
+        return con;
+    }
+
+
+    public static void main(String[] args) {
+        JFrame frame =new JFrame("Datos");
+
+        frame.setContentPane(new agenda().pagendar);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000,1000);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
+
