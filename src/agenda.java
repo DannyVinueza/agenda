952ae2224fcbd39d578aa514615b988ef32852a5
Danny Vinueza <danny.vinueza@epn.edu.ec>
2023-02-07 10:24:01 -0500

    Primer commit

---
+++ src/agenda.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class agenda {
+    private JPanel pagendar;
+    private JLabel titulo;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton buscarButton;
+    private JButton actualizarButton;
+}
